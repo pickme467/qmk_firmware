71260b90da6fd8cead11ff3b07f9c3f6eb1f5e8c
pickme467 <pawel2267@gmail.com>
2020-03-18 20:17:05 +0100

    Gherkin with modifier keys in top row

--- keyboards/40percentclub/gherkin/keymaps/pickme467/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/pickme467/keymap.c
@@ -47,48 +47,59 @@ void upper_lower_reset(qk_tap_dance_state_t* state, void* user_data) {
 
 qk_tap_dance_action_t tap_dance_actions[] =
   {
-   [UPPER_LOWER_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, upper_lower_function, upper_lower_reset, 275)
+   [UPPER_LOWER_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, upper_lower_function, upper_lower_reset, 275),
   };
 
-#define FN_X TD(UPPER_LOWER_MOD)
-#define FN_U MT(MOD_RALT, KC_U)
-#define FN_M MO(BRACES)
 #define FN_MUTE KC__MUTE
 #define FN_VOLU KC__VOLUP
 #define FN_VOLD KC__VOLDOWN
+
 #define FN_QUOT MT(MOD_LSFT, KC_QUOT)
-#define FN_SCLN MT(MOD_LCTL, KC_SCLN)
+#define FN_COMM MT(MOD_LCTL, KC_COMM)
+#define FN_DOT MT(MOD_LALT, KC_DOT)
+#define FN_P MT(MOD_LGUI, KC_P)
+#define FN_G MT(MOD_RGUI, KC_G)
+#define FN_C MT(MOD_LALT, KC_C)
+#define FN_R MT(MOD_RCTL, KC_R)
+#define FN_L MT(MOD_RSFT, KC_L)
+
+#define FN_U MT(MOD_RALT, KC_U)
+#define FN_X TD(UPPER_LOWER_MOD)
+#define FN_B KC_SPC
+
+#define FNN_M MO(BRACES)
+#define FN_SPC KC_B
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [MAIN] = LAYOUT_ortho_3x10(
-    FN_QUOT,   KC_COMM,    KC_DOT,      KC_P,      KC_Y,      KC_F,      KC_G,      KC_C,      KC_R,      KC_L,
+    FN_QUOT,   FN_COMM,    FN_DOT,      FN_P,      KC_Y,      KC_F,      FN_G,      FN_C,      FN_R,      FN_L,
        KC_A,      KC_O,      KC_E,      FN_U,      KC_I,      KC_D,      KC_H,      KC_T,      KC_N,      KC_S,
-    FN_SCLN,      KC_Q,      KC_J,      KC_K,      FN_X,    KC_SPC,      KC_M,      KC_W,      KC_V,      KC_Z
+    KC_SCLN,      KC_Q,      KC_J,      KC_K,      FN_X,      FN_B,      KC_M,      KC_W,      KC_V,      KC_Z
   ),
 
   [CURSORS] = LAYOUT_ortho_3x10(
     KC_EXLM,     KC_AT,   KC_HASH,    KC_DLR,   KC_PERC,   KC_CIRC,   KC_AMPR,   KC_ASTR,    XXXXXXX,   KC_CLCK,
     KC_HOME, KC_PGDOWN,   KC_PGUP,    KC_END,   KC_COMM,    KC_DOT,   KC_LEFT,     KC_UP,    KC_DOWN,  KC_RIGHT,
-    KC_LSFT,   KC_LALT,   KC_LCTL,    KC_LGUI,  XXXXXXX,   XXXXXXX,   KC_RGUI,   KC_RCTL,    KC_LALT,   KC_RSFT
+    KC_LSFT,   KC_LALT,   KC_LCTL,    KC_LGUI,  XXXXXXX,    FN_SPC,   KC_RGUI,   KC_RCTL,    KC_LALT,   KC_RSFT
   ),
 
   [NUMBERS] = LAYOUT_ortho_3x10(
        KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,
      KC_TAB,    KC_INS,    KC_EQL,   KC_UNDS,   KC_MINS,   KC_SLSH,   KC_BSLS,   KC_PLUS,   KC_QUES,   KC_BSPC,
-     KC_ESC,   KC_LCBR,   KC_LBRC,      KC_X,   XXXXXXX,      KC_B,      FN_M,   KC_TILD,    KC_GRV,    KC_ENT
+     KC_ESC,   KC_LCBR,   KC_LBRC,      KC_X,   XXXXXXX,    FN_SPC,      FNN_M,   KC_TILD,    KC_GRV,    KC_ENT
   ),
 
   [BRACES] = LAYOUT_ortho_3x10(
     XXXXXXX,   KC_RCBR,   KC_RBRC,   KC_RPRN,     KC_GT,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
      KC_TAB,   KC_LCBR,   KC_LBRC,   KC_LPRN,     KC_LT,   XXXXXXX,   KC_PIPE,   XXXXXXX,   XXXXXXX, KC_DELETE,
-    KC_LSFT,   KC_LALT,   KC_LCTL,   KC_LGUI,   XXXXXXX,      KC_B,   XXXXXXX,   KC_RCTL,   KC_LALT,   KC_RSFT
+    KC_LSFT,   KC_LALT,   KC_LCTL,   KC_LGUI,   XXXXXXX,    FN_SPC,   XXXXXXX,   KC_RCTL,   KC_LALT,   KC_RSFT
   ),
 
   [FUNCTIONS] = LAYOUT_ortho_3x10(
       KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,    KC_F10,
       RESET,   KC_SLCK,   FN_MUTE,   FN_VOLD,    KC_F11,    KC_F12,   FN_VOLU,   KC_VOLU,   KC_VOLD,   KC_MUTE,
-     KC_ESC,   XXXXXXX,   XXXXXXX,      KC_X,   XXXXXXX,      KC_B,   XXXXXXX,   XXXXXXX,   XXXXXXX,    KC_ENT
+     KC_ESC,   XXXXXXX,   XXXXXXX,      KC_X,   XXXXXXX,    FN_SPC,   XXXXXXX,   XXXXXXX,   XXXXXXX,    KC_ENT
   ),
 };
 
