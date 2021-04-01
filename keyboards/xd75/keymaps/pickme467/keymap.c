8b9b92b8105ba247c84dc7bcf22a4fcbbf5148da
pickme467 <pawel2267@gmail.com>
2021-04-01 20:28:29 +0200

    Added second space on the right to xd75 map

--- keyboards/xd75/keymaps/pickme467/keymap.c
+++ keyboards/xd75/keymaps/pickme467/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | END    | UP     | PG DN  | N      | M      | ,      | .      | /      | '      |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | FN     | LGUI   | LCTRL  | LSHIFT | LALT   | SPACE  | LEFT   | DOWN   | RIGHT  | RALT   | RSHIFT | RGUI   | RCTRL  | FN     | RSHIFT |
+ * | FN     | LGUI   | LCTRL  | LSHIFT | LALT   | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | RALT   | RSHIFT | RCTRL  | RGUI   | FN     |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-    MO(_FN), KC_LGUI, KC_LCTL, KC_LSFT, KC_LALT, KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_RALT, KC_RSFT, KC_RGUI, KC_RCTL, MO(_FN), KC_RSFT
+    MO(_FN), KC_LGUI, KC_LCTL, KC_LSFT, KC_LALT, KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC,  KC_RALT, KC_RSFT, KC_RCTL, KC_RGUI, MO(_FN)
   ),
 
 /* FUNCTION
@@ -55,20 +55,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | SELECT | CALC   | MYCOMP | MAIL   | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PAUSE  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | PREV   | PLAY   | NEXT   | STOP   | RGB SD | RGB SI | P4     | P5     | P6     | +      |        |        |        | RESET  |        |
+ * | PREV   | PLAY   | NEXT   | STOP   | RGB SD | RGB SI | P4     | P5     | P6     | +      |        |        |        |        | RESET  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | VOL-   | MUTE   | VOL+   | APP    | RGB VD | RGB VI | P1     | P2     | P3     | PENT   |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | FN     | RGB TG |        |        | RGB RMD| RGB MD | P0     |        | P.     | PENT   | PENT   |        |        | FN     |        |
+ * | FN     | RGB TG |        |        | RGB RMD| RGB MD | P0     |        | P.     | PENT   | PENT   |        |        |        | FN     |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
   [_FN] = LAYOUT_ortho_5x15( /* FUNCTION */
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
-    KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, _______, _______, RESET,   _______,
+    KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, _______, _______, _______, RESET,
     KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______,
-    MO(_FN), RGB_TOG, _______, _______,RGB_RMOD, RGB_MOD, KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT, _______, _______, MO(_FN), _______
+    MO(_FN), RGB_TOG, _______, _______,RGB_RMOD, RGB_MOD, KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT, _______, _______, _______, MO(_FN)
   )
 };
 
