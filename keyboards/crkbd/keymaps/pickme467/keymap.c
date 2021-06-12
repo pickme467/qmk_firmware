f1eac5f2ba74e955496783958a0f60cb5dc7b3df
pickme467 <pawel2267@gmail.com>
2021-06-12 19:27:58 +0200

    Made gui as tapdance on ralt and shift

--- keyboards/crkbd/keymaps/pickme467/keymap.c
+++ keyboards/crkbd/keymaps/pickme467/keymap.c
@@ -1,48 +1,32 @@
 #include QMK_KEYBOARD_H
 
-#define _BASE 0
-#define _LOWER 1
-#define _RAISE 2
-#define _RALT 3
+enum {
+	BASE = 0,
+	LOWER,
+  RAISE,
+	RALT,
+};
 
 enum {
-      LOWER_UPPER_MOD = 0,
-      UPPER_LOWER_MOD,
-      ALT_GUI_MOD
+	UPPER_LOWER_MOD = 0,
+	RALT_GUI_MOD,
+	LALT_SHIFT_MOD,
+	SHIFT_GUI_MOD,
 };
 
 void layer_reset(void) {
-  layer_off(_LOWER);
-  layer_off(_RAISE);
-}
-
-void lower_upper_finish(qk_tap_dance_state_t* state, void* user_data) {
-  if (state->pressed) {
-    switch (state->count) {
-    case 1:
-      layer_on(_LOWER);
-      break;
-    default:
-      layer_on(_RAISE);
-      break;
-    }
-  } else {
-    layer_reset();
-  }
-}
-
-void lower_upper_reset(qk_tap_dance_state_t* state, void* user_data) {
-  layer_reset();
+  layer_off(LOWER);
+  layer_off(RAISE);
 }
 
 void upper_lower_finish(qk_tap_dance_state_t* state, void* user_data) {
   if (state->pressed) {
     switch (state->count) {
     case 1:
-      layer_on(_RAISE);
+      layer_on(RAISE);
       break;
     default:
-      layer_on(_LOWER);
+      layer_on(LOWER);
       break;
     }
   } else {
@@ -54,45 +38,113 @@ void upper_lower_reset(qk_tap_dance_state_t* state, void* user_data) {
   layer_reset();
 }
 
-void alt_gui_reset(qk_tap_dance_state_t* state, void* user_data) {
-  switch(state->count) {
-  case 1:
-    unregister_code(KC_LALT);
-    break;
-  default:
-    unregister_code(KC_LGUI);
-    break;
-  }
+void ralt_gui_finish(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		layer_on(RALT);
+		break;
+	default:
+		register_code(KC_LGUI);
+		break;
+	}
 }
 
-void alt_gui_finish(qk_tap_dance_state_t* state, void* user_data) {
-  if (state->pressed) {
-    switch (state->count) {
-    case 1:
-      register_code(KC_LALT);
-      break;
-    default:
-      register_code(KC_LGUI);
-      break;
-    }
-   }
+void ralt_gui_reset(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		layer_off(RALT);
+		break;
+	default:
+		unregister_code(KC_LGUI);
+		break;
+	}
+}
+
+void lalt_shift_finish(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		register_code(KC_LALT);
+		break;
+	default:
+		register_code(KC_RSFT);
+		register_code(KC_LALT);
+		break;
+	}
+}
+
+void lalt_shift_reset(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		unregister_code(KC_LALT);
+		break;
+	default:
+		unregister_code(KC_RSFT);
+		unregister_code(KC_LALT);
+		break;
+	}
+}
+
+void shift_gui_finish(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		register_code(KC_RSFT);
+		break;
+	case 2:
+		register_code(KC_LGUI);
+		break;
+	case 3:
+		register_code(KC_RSFT);
+		register_code(KC_LGUI);
+		break;
+	case 4:
+		register_code(KC_LALT);
+		register_code(KC_LGUI);
+		break;
+	default:
+		register_code(KC_LALT);
+		register_code(KC_RSFT);
+		register_code(KC_LGUI);
+		break;
+	}
+}
+
+void shift_gui_reset(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		unregister_code(KC_RSFT);
+		break;
+	case 2:
+		unregister_code(KC_LGUI);
+		break;
+	case 3:
+		unregister_code(KC_RSFT);
+		unregister_code(KC_LGUI);
+		break;
+	case 4:
+		unregister_code(KC_LALT);
+		unregister_code(KC_LGUI);
+		break;
+	default:
+		unregister_code(KC_LALT);
+		unregister_code(KC_RSFT);
+		unregister_code(KC_LGUI);
+		break;
+	}
 }
 
 qk_tap_dance_action_t tap_dance_actions[] =
   {
-   [LOWER_UPPER_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, lower_upper_finish, lower_upper_reset, 275),
-   [UPPER_LOWER_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, upper_lower_finish, upper_lower_reset, 275),
-   [ALT_GUI_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, alt_gui_finish, alt_gui_reset, 275)
+		[UPPER_LOWER_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, upper_lower_finish, upper_lower_reset, 275),
+		[RALT_GUI_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, ralt_gui_finish, ralt_gui_reset, 275),
+		[LALT_SHIFT_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, lalt_shift_finish, lalt_shift_reset, 275),
+		[SHIFT_GUI_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, shift_gui_finish, shift_gui_reset, 275)
   };
 
-#define FN_DK1 TD(UPPER_LOWER_MOD)
 #define FN_DK2 TD(UPPER_LOWER_MOD)
-#define FN_LALT TD(ALT_GUI_MOD)
+#define TD_RALT TD(RALT_GUI_MOD)
+#define TD_LALT TD(LALT_SHIFT_MOD)
+#define TD_RSFT TD(SHIFT_GUI_MOD)
 
-#define FN_P MT(MOD_LGUI, KC_P)
-#define FN_G MT(MOD_RGUI, KC_G)
-
-#define FN_RALT MO(_RALT)
 #define FN_LGAL LALT(KC_LGUI)
 
 #define FN_MUTE KC__MUTE
@@ -110,44 +162,33 @@ qk_tap_dance_action_t tap_dance_actions[] =
 #define HYPER KC_F9
 #define SUPER KC_F8
 
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-  case FN_P:
-  case FN_G:
-    // GUI
-    return TAPPING_TERM * 3;
-  default:
-    return TAPPING_TERM;
-  }
-}
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT( \
-      RGB_HUD,   KC_QUOT,   KC_COMM,    KC_DOT,      FN_P,      KC_Y,             KC_F,      FN_G,      KC_C,      KC_R,      KC_L,   RGB_HUI,\
+  [BASE] = LAYOUT( \
+      RGB_HUD,   KC_QUOT,   KC_COMM,    KC_DOT,      KC_P,      KC_Y,             KC_F,      KC_G,      KC_C,      KC_R,      KC_L,   RGB_HUI,\
       RGB_SPD,      KC_A,      KC_O,      KC_E,      KC_U,      KC_I,             KC_D,      KC_H,      KC_T,      KC_N,      KC_S,   RGB_SPI,\
       RGB_SAD,   KC_SCLN,      KC_Q,      KC_J,      KC_K,      KC_X,             KC_B,      KC_M,      KC_W,      KC_V,      KC_Z,   RGB_SAI,\
-                                       FN_RALT,   KC_LCTL,    FN_DK2,          KC_LALT,    KC_SPC,   KC_RSFT\
+                                       TD_RALT,   KC_LCTL,    FN_DK2,          TD_LALT,    KC_SPC,   TD_RSFT\
   ),
 
-  [_RAISE] = LAYOUT( \
+  [RAISE] = LAYOUT( \
       RGB_VAD,      KC_1,      KC_2,      KC_3,      KC_4,      KC_5,             KC_6,      KC_7,      KC_8,      KC_9,      KC_0,   RGB_VAI,\
      RGB_RMOD,    KC_TAB,   KC_COMM,    KC_DOT,   KC_UNDS,   KC_MINS,          KC_PIPE,   KC_SLSH,   KC_PLUS,   KC_QUES,   KC_BSPC,   RGB_MOD,\
       RGB_TOG,    KC_ESC,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,          KC_RCTL,   KC_BSLS,    KC_EQL,   XXXXXXX,    KC_ENT,   RGB_TOG,\
-                                       KC_LGUI,   KC_LCTL,   XXXXXXX,          KC_LALT,    KC_SPC,   KC_RSFT\
+                                       KC_LGUI,   KC_LCTL,   XXXXXXX,          TD_LALT,    KC_SPC,   TD_RSFT\
   ),
 
-  [_LOWER] = LAYOUT( \
+  [LOWER] = LAYOUT( \
       XXXXXXX,     KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,            KC_F6,     KC_F7,     KC_F8,     KC_F9,    KC_F10,   XXXXXXX,\
       XXXXXXX,   KC_CLCK,   KC_SLCK,   FN_VOLU,   FN_MUTE,    KC_F11,           KC_F12,   XXXXXXX,   KC_PGUP,   XXXXXXX,   XXXXXXX,   XXXXXXX,\
       EEP_RST,     RESET,   XXXXXXX,   FN_VOLD,   XXXXXXX,   XXXXXXX,          XXXXXXX,   KC_HOME, KC_PGDOWN,    KC_END,    KC_ENT,   XXXXXXX,\
-                                       KC_LGUI,   KC_LCTL,   XXXXXXX,          FN_LALT,    KC_SPC,   KC_RSFT\
+                                       KC_LGUI,   KC_LCTL,   XXXXXXX,          TD_LALT,    KC_SPC,   TD_RSFT\
   ),
 
-  [_RALT] = LAYOUT( \
+  [RALT] = LAYOUT( \
       XXXXXXX, KC_INSERT,    KC_GRV,   KC_TILD,   KC_LBRC,     KC_LT,            KC_GT,   KC_RBRC,    FNRA_C,   XXXXXXX,    FNRA_L,   XXXXXXX,\
       XXXXXXX,    FNRA_A,    FNRA_O,    FNRA_E,   KC_LPRN,   KC_LCBR,          KC_RCBR,   KC_RPRN,     KC_UP,    FNRA_N,    FNRA_S,   XXXXXXX,\
       XXXXXXX, KC_DELETE,     SUPER,   KC_LSFT,     HYPER,    FNRA_X,          XXXXXXX,   KC_LEFT,   KC_DOWN,  KC_RIGHT,    FNRA_Z,   XXXXXXX,\
-                                       XXXXXXX,   KC_LCTL,   KC_LGUI,          FN_LALT,    KC_SPC,   KC_RSFT\
+                                       XXXXXXX,   KC_LCTL,   KC_LGUI,          TD_LALT,    KC_SPC,   TD_RSFT\
   )
 };
 
--- keyboards/dm9records/plaid/keymaps/pickme467/keymap.c
+++ keyboards/dm9records/plaid/keymaps/pickme467/keymap.c
@@ -8,33 +8,15 @@ enum {
 };
 
 enum {
-      LOWER_UPPER_MOD = 0,
-      UPPER_LOWER_MOD,
-      ALT_GUI_MOD
+	UPPER_LOWER_MOD = 0,
+	RALT_GUI_MOD,
+	LALT_SHIFT_MOD,
+	SHIFT_GUI_MOD,
 };
 
 void layer_reset(void) {
   layer_off(NUMBERS);
   layer_off(FUNCTIONS);
-}
-
-void lower_upper_finish(qk_tap_dance_state_t* state, void* user_data) {
-  if (state->pressed) {
-    switch (state->count) {
-    case 1:
-      layer_on(FUNCTIONS);
-      break;
-    default:
-      layer_on(NUMBERS);
-      break;
-    }
-  } else {
-    layer_reset();
-  }
-}
-
-void lower_upper_reset(qk_tap_dance_state_t* state, void* user_data) {
-  layer_reset();
 }
 
 void upper_lower_finish(qk_tap_dance_state_t* state, void* user_data) {
@@ -56,45 +38,113 @@ void upper_lower_reset(qk_tap_dance_state_t* state, void* user_data) {
   layer_reset();
 }
 
-void alt_gui_reset(qk_tap_dance_state_t* state, void* user_data) {
-  switch(state->count) {
-  case 1:
-    unregister_code(KC_LALT);
-    break;
-  default:
-    unregister_code(KC_LGUI);
-    break;
-  }
+void ralt_gui_finish(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		layer_on(RALTS);
+		break;
+	default:
+		register_code(KC_LGUI);
+		break;
+	}
 }
 
-void alt_gui_finish(qk_tap_dance_state_t* state, void* user_data) {
-  if (state->pressed) {
-    switch (state->count) {
-    case 1:
-      register_code(KC_LALT);
-      break;
-    default:
-      register_code(KC_LGUI);
-      break;
-    }
-  }
+void ralt_gui_reset(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		layer_off(RALTS);
+		break;
+	default:
+		unregister_code(KC_LGUI);
+		break;
+	}
+}
+
+void lalt_shift_finish(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		register_code(KC_LALT);
+		break;
+	default:
+		register_code(KC_RSFT);
+		register_code(KC_LALT);
+		break;
+	}
+}
+
+void lalt_shift_reset(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		unregister_code(KC_LALT);
+		break;
+	default:
+		unregister_code(KC_RSFT);
+		unregister_code(KC_LALT);
+		break;
+	}
+}
+
+void shift_gui_finish(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		register_code(KC_RSFT);
+		break;
+	case 2:
+		register_code(KC_LGUI);
+		break;
+	case 3:
+		register_code(KC_RSFT);
+		register_code(KC_LGUI);
+		break;
+	case 4:
+		register_code(KC_LALT);
+		register_code(KC_LGUI);
+		break;
+	default:
+		register_code(KC_LALT);
+		register_code(KC_RSFT);
+		register_code(KC_LGUI);
+		break;
+	}
+}
+
+void shift_gui_reset(qk_tap_dance_state_t* state, void* user_data) {
+	switch (state->count) {
+	case 1:
+		unregister_code(KC_RSFT);
+		break;
+	case 2:
+		unregister_code(KC_LGUI);
+		break;
+	case 3:
+		unregister_code(KC_RSFT);
+		unregister_code(KC_LGUI);
+		break;
+	case 4:
+		unregister_code(KC_LALT);
+		unregister_code(KC_LGUI);
+		break;
+	default:
+		unregister_code(KC_LALT);
+		unregister_code(KC_RSFT);
+		unregister_code(KC_LGUI);
+		break;
+	}
 }
 
 qk_tap_dance_action_t tap_dance_actions[] =
   {
-   [LOWER_UPPER_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, lower_upper_finish, lower_upper_reset, 275),
-   [UPPER_LOWER_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, upper_lower_finish, upper_lower_reset, 275),
-   [ALT_GUI_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, alt_gui_finish, alt_gui_reset, 275)
+		[UPPER_LOWER_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, upper_lower_finish, upper_lower_reset, 275),
+		[RALT_GUI_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, ralt_gui_finish, ralt_gui_reset, 275),
+		[LALT_SHIFT_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, lalt_shift_finish, lalt_shift_reset, 275),
+		[SHIFT_GUI_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, shift_gui_finish, shift_gui_reset, 275)
   };
 
-#define FN_DK1 TD(UPPER_LOWER_MOD)
 #define FN_DK2 TD(UPPER_LOWER_MOD)
-#define FN_LALT TD(ALT_GUI_MOD)
+#define TD_RALT TD(RALT_GUI_MOD)
+#define TD_LALT TD(LALT_SHIFT_MOD)
+#define TD_RSFT TD(SHIFT_GUI_MOD)
 
-#define FN_P MT(MOD_LGUI, KC_P)
-#define FN_G MT(MOD_RGUI, KC_G)
-
-#define FN_RALT MO(RALTS)
 #define FN_LGAL LALT(KC_LGUI)
 
 #define FNRA_A RALT(KC_A)
@@ -109,44 +159,33 @@ qk_tap_dance_action_t tap_dance_actions[] =
 #define HYPER KC_F9
 #define SUPER KC_F8
 
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-  case FN_P:
-  case FN_G:
-    // GUI
-    return TAPPING_TERM * 3;
-  default:
-    return TAPPING_TERM;
-  }
-}
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_plaid_grid(
-    KC_QUOT,   KC_COMM,    KC_DOT,      FN_P,      KC_Y,   XXXXXXX,   XXXXXXX,      KC_F,      FN_G,      KC_C,      KC_R,      KC_L,
+    KC_QUOT,   KC_COMM,    KC_DOT,      KC_P,      KC_Y,   XXXXXXX,   XXXXXXX,      KC_F,      KC_G,      KC_C,      KC_R,      KC_L,
        KC_A,      KC_O,      KC_E,      KC_U,      KC_I,   XXXXXXX,   XXXXXXX,      KC_D,      KC_H,      KC_T,      KC_N,      KC_S,
     KC_SCLN,      KC_Q,      KC_J,      KC_K,      KC_X,   XXXXXXX,   XXXXXXX,      KC_B,      KC_M,      KC_W,      KC_V,      KC_Z,
-    XXXXXXX,   XXXXXXX,   FN_RALT,   KC_LCTL,    FN_DK2,   XXXXXXX,   XXXXXXX,   KC_LALT,    KC_SPC,   KC_RSFT,   XXXXXXX,   XXXXXXX
+    XXXXXXX,   XXXXXXX,   TD_RALT,   KC_LCTL,    FN_DK2,   KC_LGUI,   KC_RGUI,   TD_LALT,    KC_SPC,   TD_RSFT,   XXXXXXX,   XXXXXXX
   ),
 
   [NUMBERS] = LAYOUT_plaid_grid(
        KC_1,      KC_2,      KC_3,      KC_4,      KC_5,   XXXXXXX,   XXXXXXX,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,
      KC_TAB,   KC_COMM,    KC_DOT,   KC_UNDS,   KC_MINS,   XXXXXXX,   XXXXXXX,   KC_PIPE,   KC_SLSH,   KC_PLUS,   KC_QUES,   KC_BSPC,
      KC_ESC,   XXXXXXX,   XXXXXXX,      KC_X,   XXXXXXX,   XXXXXXX,   XXXXXXX,      KC_B,   KC_BSLS,    KC_EQL,   XXXXXXX,    KC_ENT,
-    XXXXXXX,   XXXXXXX,   KC_LGUI,   KC_LCTL,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LALT,    KC_SPC,   KC_RSFT,   XXXXXXX,   XXXXXXX
+    XXXXXXX,   XXXXXXX,   KC_LGUI,   KC_LCTL,   XXXXXXX,   KC_LGUI,   KC_RGUI,   TD_LALT,    KC_SPC,   TD_RSFT,   XXXXXXX,   XXXXXXX
   ),
 
   [FUNCTIONS] = LAYOUT_plaid_grid(
       KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,   XXXXXXX,   XXXXXXX,     KC_F6,     KC_F7,     KC_F8,     KC_F9,    KC_F10,
     KC_CLCK,   KC_SLCK,   KC_VOLU,   KC_MUTE,    KC_F11,   XXXXXXX,   XXXXXXX,    KC_F12,   XXXXXXX,   KC_PGUP,   XXXXXXX,   XXXXXXX,
       RESET,   XXXXXXX,   KC_VOLD,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_HOME, KC_PGDOWN,    KC_END,    KC_ENT,
-    XXXXXXX,   XXXXXXX,   KC_LGUI,   KC_LCTL,   XXXXXXX,   XXXXXXX,   XXXXXXX,   FN_LALT,    KC_SPC,   KC_RSFT,   XXXXXXX,   XXXXXXX
+    XXXXXXX,   XXXXXXX,   KC_LGUI,   KC_LCTL,   XXXXXXX,   KC_LGUI,   KC_RGUI,   TD_LALT,    KC_SPC,   TD_RSFT,   XXXXXXX,   XXXXXXX
   ),
 
   [RALTS] = LAYOUT_plaid_grid(
   KC_INSERT,    KC_GRV,   KC_TILD,   KC_LBRC,     KC_LT,   XXXXXXX,   XXXXXXX,     KC_GT,   KC_RBRC,    FNRA_C,    XXXXXXX,    FNRA_L,
      FNRA_A,    FNRA_O,    FNRA_E,   KC_LPRN,   KC_LCBR,   XXXXXXX,   XXXXXXX,   KC_RCBR,   KC_RPRN,     KC_UP,     FNRA_N,    FNRA_S,
   KC_DELETE,     SUPER,   KC_LSFT,     HYPER,    FNRA_X,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LEFT,   KC_DOWN,   KC_RIGHT,    FNRA_Z,
-    XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LCTL,   KC_LGUI,   XXXXXXX,   XXXXXXX,   FN_LALT,    KC_SPC,   KC_RSFT,    XXXXXXX,   XXXXXXX
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LCTL,   KC_LGUI,   KC_LGUI,   KC_RGUI,   TD_LALT,    KC_SPC,   TD_RSFT,    XXXXXXX,   XXXXXXX
   )
 };
 
@@ -209,8 +248,7 @@ const uint16_t modifiers[] = {
     KC_LGUI,
     KC_RGUI,
     FN_DK2,
-    FN_RALT,
-    FN_LALT
+    TD_RALT
 };
 
 
