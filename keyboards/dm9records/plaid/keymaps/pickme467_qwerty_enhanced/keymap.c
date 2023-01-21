d4374a0a760da622b75ff565b207e68e41a96b14
pickme467 <pawel2267@gmail.com>
2023-01-21 14:34:23 +0100

    Moved around a reset button on the adjust layer.

--- keyboards/dm9records/plaid/keymaps/pickme467_qwerty_enhanced/keymap.c
+++ keyboards/dm9records/plaid/keymaps/pickme467_qwerty_enhanced/keymap.c
@@ -161,20 +161,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |Reset |      |      |      |      |      |      |      |      |      |      | Del  |
+ * |      |      |      |      |      |      |      |      |      |      |      | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |Caps  |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|      | Ins  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |PrnScr|ScrLck|Pause |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |Reset |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_plaid_grid(
-    QK_BOOT, LED_1,   LED_2,   LED_3,   LED_4,   LED_5,   LED_6,   LED_7,   LED_8,   LED_9,   LED_0,   KC_DEL,
+    _______, LED_1,   LED_2,   LED_3,   LED_4,   LED_5,   LED_6,   LED_7,   LED_8,   LED_9,   LED_0,   KC_DEL,
     KC_CAPS, XXXXXXX, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  XXXXXXX, KC_INS,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  XXXXXXX, KC_PSCR, KC_SCRL, KC_PAUS, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 
